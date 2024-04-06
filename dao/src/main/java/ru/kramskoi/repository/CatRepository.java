b19f0ec4ed700e9377ad2cb61bc6c87bead37cb6
lilpipidron <lociX55Lim@gmail.com>
2024-04-06 17:00:15 +0300

    refactor: cleanup

--- dao/src/main/java/ru/kramskoi/repository/CatRepository.java
+++ dao/src/main/java/ru/kramskoi/repository/CatRepository.java
@@ -1,8 +1,6 @@
 package ru.kramskoi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.kramskoi.breeds.Breed;
 import ru.kramskoi.colors.Color;
