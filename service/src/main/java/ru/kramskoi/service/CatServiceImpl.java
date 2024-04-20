bc74ac9d12be2780d0ba3025cc37c9455bc357e0
lilpipidron <lociX55Lim@gmail.com>
2024-04-20 13:22:48 +0300

    feat: use streamApi

--- service/src/main/java/ru/kramskoi/service/CatServiceImpl.java
+++ service/src/main/java/ru/kramskoi/service/CatServiceImpl.java
@@ -1,7 +1,6 @@
 package ru.kramskoi.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Service;
 import ru.kramskoi.breeds.Breed;
 import ru.kramskoi.colors.Color;
@@ -10,87 +9,79 @@ import ru.kramskoi.entity.Cat;
 import ru.kramskoi.mapper.CatMapper;
 import ru.kramskoi.repository.CatRepository;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CatServiceImpl implements CatService {
-  @Autowired
-  CatRepository catRepository;
+    @Autowired
+    CatRepository catRepository;
 
-  @Override
-  public void addCat(Cat cat) {
-    catRepository.save(cat);
-  }
-
-  @Override
-  public CatDTO findCatByID(Long id) {
-    return CatMapper.fromCatToDTO(catRepository.getCatById(id));
-  }
-
-  @Override
-  public List<CatDTO> findCatsByColor(Color color) {
-    List<CatDTO> catDTOS = new ArrayList<>();
-    List<Cat> cats = catRepository.getCatsByColor(color);
-    for (Cat cat : cats) {
-      catDTOS.add(CatMapper.fromCatToDTO(cat));
+    @Override
+    public void addCat(Cat cat) {
+        catRepository.save(cat);
     }
-    return catDTOS;
-  }
 
-  @Override
-  public List<CatDTO> findCatsByBreed(Breed breed) {
-    List<CatDTO> catDTOS = new ArrayList<>();
-    List<Cat> cats = catRepository.getCatsByBreed(breed);
-    for (Cat cat : cats) {
-      catDTOS.add(CatMapper.fromCatToDTO(cat));
+    @Override
+    public CatDTO findCatByID(Long id) {
+        return CatMapper.fromCatToDTO(catRepository.getCatById(id));
     }
-    return catDTOS;
-  }
 
-  @Override
-  public List<CatDTO> getFriendsById(Long id) {
-    List<CatDTO> catDTOS = new ArrayList<>();
-    Cat cat = catRepository.getCatById(id);
-    List<Cat> friends = cat.getFriends();
-    for (Cat friend : friends) {
-      catDTOS.add(CatMapper.fromCatToDTO(friend));
+    @Override
+    public List<CatDTO> findCatsByColor(Color color) {
+        return catRepository.getCatsByColor(color)
+                .stream()
+                .map(CatMapper::fromCatToDTO)
+                .collect(Collectors.toList());
     }
-    return catDTOS;
-  }
 
-  @Override
-  public void addFriend(Long catId, Long friendId) {
-    Cat cat = catRepository.getCatById(catId);
-    Cat friend = catRepository.getCatById(friendId);
-    cat.addFriend(friend);
-    friend.addFriend(cat);
-    catRepository.save(cat);
-    catRepository.save(friend);
-  }
-
-  @Override
-  public List<CatDTO> getAllCatsByOwnerId(Long id) {
-    List<CatDTO> catDTOS = new ArrayList<>();
-    List<Cat> cats = catRepository.getCatsByOwnerId(id);
-    for (Cat cat : cats) {
-      catDTOS.add(CatMapper.fromCatToDTO(cat));
+    @Override
+    public List<CatDTO> findCatsByBreed(Breed breed) {
+        return catRepository.getCatsByBreed(breed)
+                .stream()
+                .map(CatMapper::fromCatToDTO)
+                .collect(Collectors.toList());
     }
-    return catDTOS;
-  }
 
-  @Override
-  public void updateCat(Cat cat) {
-    Cat prevCat = catRepository.getCatById(cat.getId());
-    prevCat.setName(cat.getName());
-    prevCat.setBirthday(cat.getBirthday());
-    prevCat.setBreed(cat.getBreed());
-    prevCat.setColor(cat.getColor());
-    catRepository.save(cat);
-  }
+    @Override
+    public List<CatDTO> getFriendsById(Long id) {
+        return catRepository.getCatById(id)
+                .getFriends()
+                .stream()
+                .map(CatMapper::fromCatToDTO)
+                .collect(Collectors.toList());
+    }
 
-  @Override
-  public void deleteCat(Long id) {
-    catRepository.deleteById(id);
-  }
+    @Override
+    public void addFriend(Long catId, Long friendId) {
+        Cat cat = catRepository.getCatById(catId);
+        Cat friend = catRepository.getCatById(friendId);
+        cat.addFriend(friend);
+        friend.addFriend(cat);
+        catRepository.save(cat);
+        catRepository.save(friend);
+    }
+
+    @Override
+    public List<CatDTO> getAllCatsByOwnerId(Long id) {
+        return catRepository.getCatsByOwnerId(id)
+                .stream()
+                .map(CatMapper::fromCatToDTO)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public void updateCat(Cat cat) {
+        Cat prevCat = catRepository.getCatById(cat.getId());
+        prevCat.setName(cat.getName());
+        prevCat.setBirthday(cat.getBirthday());
+        prevCat.setBreed(cat.getBreed());
+        prevCat.setColor(cat.getColor());
+        catRepository.save(cat);
+    }
+
+    @Override
+    public void deleteCat(Long id) {
+        catRepository.deleteById(id);
+    }
 }
