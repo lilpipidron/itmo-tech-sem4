edce01767c366317069773899a2796dc4dc4f24b
vadim <lociX55Lim@gmail.com>
2024-05-24 11:48:07 +0300

    feat: add owner module

--- owner/build.gradle.kts
+++ owner/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("java")
+    java
 }
 
 group = "ru.kramskoi"
@@ -14,18 +14,23 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("junit:junit:4.13.1")
 
-    implementation(project(":service"))
     implementation(project(":dao"))
 
-
     implementation("org.springframework.boot:spring-boot-starter-validation:3.0.6")
-    implementation("org.springframework.data:spring-data-jpa:3.2.2")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor:3.0.6")
     testImplementation("org.springframework.boot:spring-boot-starter-test:3.0.6")
-    implementation("org.springframework.boot:spring-boot-starter-web:3.0.6")
+    implementation("org.springframework.data:spring-data-jpa:3.0.6")
+
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok:1.18.22")
+    implementation("org.projectlombok:lombok:1.18.22")
+
+    implementation("com.fasterxml.jackson.core:jackson-annotations:2.2.1")
 
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor:3.0.6")
     implementation("org.springframework.boot:spring-boot-starter-security:3.0.6")
 
+    implementation("at.favre.lib:bcrypt:0.9.0")
 }
 
 tasks.test {
