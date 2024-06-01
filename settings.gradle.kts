739cf3797c0b6d4bc70bc5caf1c0fa492c54e506
vadim <lociX55Lim@gmail.com>
2024-06-01 19:02:18 +0300

    fix

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,19 +1,14 @@
 include("Bank")
 include("Console")
-include("dao")
-include("service")
-include("controller")
 include("api-gateway")
 include("cat")
 include("owner")
-include("cat-client")
-include("owner-client")
 include("domain")
 include("cat:cat-core")
 findProject(":cat:cat-core")?.name = "cat-core"
 include("owner:owner-core")
 findProject(":owner:owner-core")?.name = "owner-core"
-include("cat:client")
-findProject(":cat:client")?.name = "client"
-include("owner:client")
-findProject(":owner:client")?.name = "client"
+include("cat:cat-client")
+findProject(":cat:cat-client")?.name = "cat-client"
+include("owner:owner-client")
+findProject(":owner:owner-client")?.name = "owner-client"
